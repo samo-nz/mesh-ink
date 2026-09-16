89584b607d92ce190f88dd11102a7ac50ccd8845
samo-nz <samo.ubuntu@gmail.com>
2026-09-16 15:04:05 +1200

    Release 0.0.1 with full panel clear and BQ27220 battery voltage

--- src/board/target.h
+++ src/board/target.h
@@ -8,6 +8,7 @@
 class T5Board : public ESP32Board {
 public:
     void begin();
+    uint16_t getBattMilliVolts() override;
     const char* getManufacturerName() const override { return "LILYGO T5 E-Paper S3 Pro"; }
 };
 
