86690d3972b22187c8c656daefbf511f0a77f05f
samo-nz <samo.ubuntu@gmail.com>
2026-09-24 10:50:56 +1200

    Stub internal RAM flag used by PMTiles directory staging

--- tests/pmtiles_stubs/esp_heap_caps.h
+++ tests/pmtiles_stubs/esp_heap_caps.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #define MALLOC_CAP_SPIRAM 1
 #define MALLOC_CAP_8BIT 2
+#define MALLOC_CAP_INTERNAL 4
 static inline void* heap_caps_malloc(size_t n, int) { return malloc(n); }
 
 static inline bool heap_caps_check_integrity_all(bool) { return true; }
