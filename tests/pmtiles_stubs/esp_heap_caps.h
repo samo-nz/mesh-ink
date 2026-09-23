c3c2c78401ad686a532ac69e5fd7166e5fff1790
samo-nz <samo.ubuntu@gmail.com>
2026-09-24 10:22:46 +1200

    Stub integrity check in host PMTiles tests

--- tests/pmtiles_stubs/esp_heap_caps.h
+++ tests/pmtiles_stubs/esp_heap_caps.h
@@ -3,3 +3,5 @@
 #define MALLOC_CAP_SPIRAM 1
 #define MALLOC_CAP_8BIT 2
 static inline void* heap_caps_malloc(size_t n, int) { return malloc(n); }
+
+static inline bool heap_caps_check_integrity_all(bool) { return true; }
