3ab5219b670a1a1be70e340ed453eb5451350e87
samo-nz <samo.ubuntu@gmail.com>
2026-09-26 22:08:17 +1200

    Profile mesh and UI loop timing in testing build

--- src/unified_main.cpp
+++ src/unified_main.cpp
@@ -2,6 +2,7 @@
 #include <Preferences.h>
 #include "ui_onboarding.h"
 #include "companion_runtime.h"
+#include "t5_timing.h"
 
 static bool companion_mode = false;
 static constexpr uint8_t BOOT_BUTTON = 0;
@@ -57,7 +58,16 @@ void loop() {
         companion_loop();
         companion_exit_button();
     } else {
-        if(local_mesh_is_running())local_mesh_loop();
+        const uint32_t cycle_started=t5_timing_cycle_begin();
+        if(local_mesh_is_running()){
+            const uint32_t mesh_started=t5_timing_section_begin(T5TimingSection::Mesh);
+            local_mesh_loop();
+            t5_timing_section_end(T5TimingSection::Mesh,mesh_started);
+        }
+        const uint32_t ui_started=t5_timing_section_begin(T5TimingSection::Ui);
         ui_loop();
+        t5_timing_section_end(T5TimingSection::Ui,ui_started);
+        t5_timing_cycle_end(cycle_started);
+        t5_timing_service();
     }
 }
